65f549d5d7abc467e7c1edee0a46f896e1cb3d30
Marcelo Lima <limarcelo90@gmail.com>
2017-04-15 03:31:44 -0400

    atualizando arquivos bootstrap e tomcat

--- tcc/src/main/java/br/com/projetotcc/StartController.java
+++ tcc/src/main/java/br/com/projetotcc/StartController.java
@@ -1,14 +1,19 @@
 package br.com.projetotcc;
 
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StartController {
 	
-	@RequestMapping("/")
+	@RequestMapping("/home")
 	public String home(){
-		return "login";
+		return "home";
 		
 	}
+	
+	
+        
 }
