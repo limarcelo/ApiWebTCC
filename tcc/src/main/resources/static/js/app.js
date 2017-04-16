33e5a66fb38a0c0a8adf2f8342c07c83be8d6e7e
Marcelo Lima <limarcelo90@gmail.com>
2017-04-16 19:51:23 -0400

    app.js

--- tcc/src/main/resources/static/js/app.js
+++ tcc/src/main/resources/static/js/app.js
@@ -1,7 +1,20 @@
 var picolApp = angular.module("picolApp", []);
 
-picolApp.controller("loginController", function($scope){
-	
+picolApp.controller("loginController", function($scope, $http){
 	$scope.usuario = "Jão";
 	
+	$http({
+		  method: 'GET',
+		  url: '/usuario/listar'
+		}).then(function successCallback(response) {
+		    console.log(reponse.data);
+		    onsole.log(reponse.status);
+		  }, function errorCallback(response) {
+			    console.log(reponse.data);
+			    console.log(reponse.status);
+		  });
+	
+	
 });
+
+
